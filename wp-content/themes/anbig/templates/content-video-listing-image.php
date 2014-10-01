b5a758c4b751ef91191394586428ef2ab87932a4
Ron Chan <ksc.ron@gmail.com>
2014-10-02 03:59:50 +0800

    pagination style

--- wp-content/themes/anbig/templates/content-video-listing-image.php
+++ wp-content/themes/anbig/templates/content-video-listing-image.php
@@ -43,16 +43,18 @@
   endwhile;
  ?>
  	</div>
-  <div class="row">
-<?
-  $big = 999999999; // need an unlikely integer
-
-  echo paginate_links( array(
-  'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-  'format' => '?paged=%#%',
-  'current' => max( 1, get_query_var('paged') ),
-  'total' => $wp_query->max_num_pages
-) );
-?>
+  <div class="row textAlignCenter">
+  	<div class="paginationContainer">
+		<?
+          $big = 999999999; // need an unlikely integer
+        
+          echo paginate_links( array(
+          'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+          'format' => '?paged=%#%',
+          'current' => max( 1, get_query_var('paged') ),
+          'total' => $wp_query->max_num_pages
+        ) );
+        ?>
+	</div>
   </div>
 </div>
