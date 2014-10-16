d5f0c0a34cccc5b0f5d74ca1127cf5b02c258a11
Ron Chan <ksc.ron@gmail.com>
2014-10-16 15:18:00 +0800

    search page and function

--- wp-content/themes/anbig/templates/content-search-result.php
+++ wp-content/themes/anbig/templates/content-search-result.php
@@ -1,37 +1,45 @@
   <?php //the_content(); ?>
   <div class="container" style="margin-top:15px;">
-  	<? if($post->post_type == "video"){?>
-    	
-    	<div class="row">
-			<?
-			  $i=0;
-              while ( $wp_query->have_posts() ) :
-                $wp_query->the_post();
-				$id = get_the_ID();
-				$post = get_post($id);
-				$video_id = get_field('video');
-				$video_url = wp_get_attachment_url( $video_id );
-				$image_url = wp_get_attachment_image_src( get_post_thumbnail_id($id), 'listing-image' );
-             	if($i%3==0){?>
-                    <div class="row">
-                <? } ?>
-                <div class="col-sm-4 listingImageItem clearfix">
-                    <div class="videoThumbContainer">
-                      <a class="videoLink" href="<?=$video_url?>" person="<?=get_field("doctor",$result->ID)?>" desp="<?=the_content()?>"><img class="img-responsive fullwidthImg" src="<?=$image_url[0]?>" /></a>
-                      <a class="videoLink btnVideoPlay" href="<?=$video_url?>" person="<?=get_field("doctor",$result->ID)?>" desp="<?=the_content()?>"></a>
-                    </div>
-                    <p class="doctor"><?=get_field("doctor",$result->ID)?></p>
-                    <p class="desp"><? $content = get_the_content(); $content = strip_tags($content); echo substr($content, 0, 90).'...'; ?></p>
-                    <a class="btnReadMore videoLink" href="<?=$video_url?>" person="<?=get_field("doctor",$result->ID)?>" desp="<?=the_content()?>">Read more</a>
-                </div>
-             <? if($i%3==2){ ?>
+  	<? if($post->post_type == "video"){
+		  $i=0;
+		  while ( $wp_query->have_posts() ) :
+			$wp_query->the_post();
+			$id = get_the_ID();
+			$post = get_post($id);
+			$video_id = get_field('video');
+			$video_url = wp_get_attachment_url( $video_id );
+			$image_url = wp_get_attachment_image_src( get_post_thumbnail_id($id), 'listing-image' );
+			if($i%3==0){?>
+				<div class="row">
+			<? } ?>
+			<div class="col-sm-4 listingImageItem clearfix">
+				<div class="videoThumbContainer">
+				  <a class="videoLink" href="<?=$video_url?>" person="<?=get_field("doctor",$result->ID)?>" desp="<?=the_content()?>"><img class="img-responsive fullwidthImg" src="<?=$image_url[0]?>" /></a>
+				  <a class="videoLink btnVideoPlay" href="<?=$video_url?>" person="<?=get_field("doctor",$result->ID)?>" desp="<?=the_content()?>"></a>
 				</div>
-			 <? }	
-			  $i++; 
-              endwhile;
-             ?>
-		</div>
-   
+				<p class="doctor"><?=get_field("doctor",$result->ID)?></p>
+				<p class="desp"><? $content = get_the_content(); $content = strip_tags($content); echo substr($content, 0, 90).'...'; ?></p>
+				<a class="btnReadMore videoLink" href="<?=$video_url?>" person="<?=get_field("doctor",$result->ID)?>" desp="<?=the_content()?>">Read more</a>
+			</div>
+		 <? if($i%3==2){ ?>
+			</div>
+		 <? }	
+		  $i++; 
+		  endwhile;
+		 ?>
+         
+         <div class="row">
+			<?
+              $big = 999999999; // need an unlikely integer
+            
+              echo paginate_links( array(
+              'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+              'format' => '?paged=%#%',
+              'current' => max( 1, get_query_var('paged') ),
+              'total' => $wp_query->max_num_pages
+            ) );
+            ?>
+          </div>  
     <? } ?>
     
     <? if($post->post_type == "image"){
@@ -57,9 +65,21 @@
 	        </div>
     	    <? }
 			$i++;
-			endwhile;
-			
-   } ?>
+			endwhile; ?>
+            
+            <div class="row">
+				<?
+                  $big = 999999999; // need an unlikely integer
+                
+                  echo paginate_links( array(
+                  'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                  'format' => '?paged=%#%',
+                  'current' => max( 1, get_query_var('paged') ),
+                  'total' => $wp_query->max_num_pages
+                ) );
+                ?>
+           </div> 			
+  <? } ?>
   </div>
 
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
