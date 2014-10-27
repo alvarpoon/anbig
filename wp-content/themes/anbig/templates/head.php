bb3260c61caae5182fda447d6619f0affa5fe2f8
SedgwickRichardson <michiel@sedgwick-richardson.com>
2014-10-27 14:37:04 +0800

    added response.js

--- wp-content/themes/anbig/templates/head.php
+++ wp-content/themes/anbig/templates/head.php
@@ -7,6 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <?php wp_head(); ?>
-
+  <!--[if lt IE 9]>
+    <script type="text/javascript" src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script type="text/javascript" src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
 </head>
